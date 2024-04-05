71844c7cc5dbb7378ccd05f2bcb20a005d97a121
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-04-05 20:03:04 -0300

    Version 1.0.2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <x-select id="select-user" :options="$options" :selected="$selectedOption" :text="'- Select the User -'" />
-                    @if ( isset($options) && count($options) == 1 )
+                    @if ( isset($options) && count($options) == 0 )
                         <div class="text-red-600 mb-3">
                             To proceed with testing, you'll need to register additional users. Please open an incognito or private browsing window and navigate to the <a href="{{ route('register') }}" class="underline">Registration Page</a> to create new accounts.
                         </div>

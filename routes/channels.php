2ab589b14612565c1b02114244abe6cef88d28ae
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-04-09 19:36:55 -0300

    Version 1.0.4

--- routes/channels.php
+++ routes/channels.php
@@ -5,8 +5,15 @@ use App\Models\User;
 use App\Models\Chat;
 
 /**
- * Authorizing Channels
+ * Authorizes users to access a private chat channel based on the channel ID.
+ * The channel ID is expected to be in the format 'chat-channel.userId1_userId2'.
+ * Only users with IDs matching userId1 or userId2 are authorized to join this channel.
+ * This ensures that only the two users involved in a chat can listen to the channel.
  * DOC: https://laravel.com/docs/11.x/broadcasting#example-application-authorizing-channels
+ *
+ * @param User $user The currently authenticated user.
+ * @param string $ids A string containing the user IDs involved in the chat, separated by an underscore.
+ * @return array|bool Returns user details if authorized, otherwise false.
  */
 Broadcast::channel('chat-channel.{ids}', function (User $user, $ids) {
     [$userId1, $userId2] = explode('_', $ids);
@@ -19,8 +26,15 @@ Broadcast::channel('chat-channel.{ids}', function (User $user, $ids) {
     return false;
 }, ['guards' => ['web']]);
 
-
-// List online users.
+/**
+ * Authorizes users to access the 'users-online' presence channel.
+ * This channel is used to track which users are currently online.
+ * Only authenticated users are allowed to join this channel.
+ * Each user's ID and name are broadcast to others in the channel.
+ *
+ * @param User $user The currently authenticated user.
+ * @return array|bool Returns user details if authorized, otherwise false.
+ */
 Broadcast::channel('users-online', function (User $user) {
     if (auth()->check()) {
         return [
@@ -30,4 +44,3 @@ Broadcast::channel('users-online', function (User $user) {
     }
     return false;
 }, ['guards' => ['web']]);
-
